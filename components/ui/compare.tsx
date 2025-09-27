23183f7132e256b932849fc456ee96b09adcee37
gapinsky <gapinski02@gmail.com>
2025-09-27 21:43:17 +0200

    fixes

--- components/ui/compare.tsx
+++ components/ui/compare.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect, useRef, useCallback } from "react";
 import { SparklesCore } from "@/components/ui/sparkles";
 import { AnimatePresence, motion } from "motion/react";
 import { cn } from "@/lib/utils";
-import { IconDotsVertical } from "@tabler/icons-react";
 import { UnfoldHorizontal } from "lucide-react";
 interface CompareProps {
   firstImage?: string;
